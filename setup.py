dfad1dcb9b8cb76e7d9035471eec53c26377a18e
Wufei Ma <wufeim@gmail.com>
2023-01-29 13:55:01 -0500

    init update

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='dnnlib',
+    version='1.0.0',
+    description='dnnlib libruary by NVlabs',
+    author='NVIDIA',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=['dnnlib'],
+)
